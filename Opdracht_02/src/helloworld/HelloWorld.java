888ee53deaefe22878d2966349a2d284291126ff
axelcolsonUH <axel.colson@student.uhasselt.be>
2020-09-22 12:43:01 +0200

    Opdracht_02_02: oefening7

--- Opdracht_02/src/helloworld/HelloWorld.java
+++ Opdracht_02/src/helloworld/HelloWorld.java
@@ -3,7 +3,17 @@ package helloworld;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, world");
+		System.out.println("Hello, world"+ "\n");
+		String file = args[args.length-1];
+		String[] values = new String[args.length];
+		for (int i = 0; i < args.length; i++) {
+			values[i] = (args[i]);
+		}
+		System.out.println(file + "\n");
+		
+		for (int i = 0; i < values.length; i++) {
+			System.out.println(values[i]);
+		}
 
 	}
 
