598619a56d418422f1cddceb398d2fde34a5fd97
axelcolsonUH <axel.colson@student.uhasselt.be>
2020-09-22 13:25:36 +0200

    Project cleanup + opdracht 03 finished. Opdracht 04 & 05 created

--- Opdracht_02_02/src/helloworld/HelloWorld.java
+++ Opdracht_02_02/src/helloworld/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello, world"+ "\n");
+		
 		String file = args[args.length-1];
 		String[] values = new String[args.length];
 		for (int i = 0; i < args.length; i++) {
---
+++ Opdracht_02_03/src/Matrix.java
@@ -0,0 +1,55 @@
+
+public class Matrix {
+
+	public static void main(String[] args) {
+		int[][] m1 = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+		int[][] m2 = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+
+		printMatrix(m1);
+		printMatrix(m2);
+
+		int[][] m3 = AddMatrix(m1, m2);
+		printMatrix(m3);
+
+		int[][] m4 = multiplyMatrix(m1, m2);
+		printMatrix(m4);
+
+	}
+
+	public static void printMatrix(int[][] matrix) {
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix[i].length; j++) {
+				System.out.print(matrix[i][j] + " ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+	}
+
+	public static int[][] AddMatrix(int[][] matrix1, int[][] matrix2) {
+		int[][] c = new int[3][3];
+
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				c[i][j] = matrix1[i][j] + matrix2[i][j]; 
+			}
+		}
+
+		return c;
+	}
+
+	public static int[][] multiplyMatrix(int[][] matrix1, int[][] matrix2) {
+		int[][] c = new int[3][3];
+
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				c[i][j] = 0;
+				for (int k = 0; k < 3; k++) {
+					c[i][j] += matrix1[i][k] * matrix2[k][j];
+				}
+			}
+		}
+		return c;
+	}
+
+}
