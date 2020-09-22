b023b0520bde4379a5dcf15c729ad681c083ddab
axelcolsonUH <axel.colson@student.uhasselt.be>
2020-09-22 14:10:17 +0200

    Finished Opdracht_02_05

---
+++ Opdracht_02_05/src/opdracht_02_05/Person.java
@@ -0,0 +1,20 @@
+package opdracht_02_05;
+
+public class Person {
+	private String voornaam;
+	private String naam;
+	private String gender;
+	
+	public Person() {
+		
+	}
+	
+	public Person(String voornaam, String naam, String gender) {
+		this.voornaam = voornaam;
+		this.naam = naam;
+		this.gender = gender;
+	}
+	
+	
+
+}
